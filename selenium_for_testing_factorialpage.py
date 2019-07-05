91cd2a1dca7a2789af0e66c20e999c4543a603d2
akkuldn <akkul.dn@qxf2.com>
2019-07-05 16:56:15 +0530

    I have commited the changes to the program

--- selenium_for_testing_factorialpage.py
+++ selenium_for_testing_factorialpage.py
@@ -18,20 +18,21 @@ if(driver.title=="Factoriall"):
     print("The title is correct and we have accessed the correct page")
 else:
     print("failedThe title does not match. Wrong page has loaded")
-input_value = "asd"
+input_value = "abc"
 text=driver.find_element_by_xpath("//input[@id='number']")
 
 text.send_keys(input_value)
 driver.find_element_by_xpath("//button[@id='getFactorial']").click()
 time.sleep(4)
 result=driver.find_element_by_xpath("//p[@id='resultDiv']").text
-print(result)
-if(type(result=='int')):
-    res=int(result.split( )[-1])
+print("the result is",result)
+res=result.split( )[-1])
 print(res)
-if(res==math.factorial(input_value)):
-    print("The obtained result match the exepected result.Therefore the test is successfull")
+if(type(res==int)):
+    if(res==math.factorial(input_value)):
+        print("The obtained result match the exepected result.Therefore the test is successfull")
+    else:
+        print("The obtained result does not match the expected result.Therefore the test is failed")
 else:
-    print("The obtained result does not match the expected result.Therefore the test is failed")
-
+    print("Please enter an integer value")
 driver.close()
