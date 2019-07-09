6fde1401014e6d4eb590d4e9a5031d9537fe209e
akkuldn <akkul.dn@qxf2.com>
2019-07-09 13:22:08 +0530

    I have designed program to add most expensive moisturizer into the cart

--- Most_expensive_moisturizer.py
+++ Most_expensive_moisturizer.py
@@ -8,11 +8,26 @@ from selenium import webdriver
 driver=webdriver.Chrome()
 driver.get("https://weathershopper.pythonanywhere.com/moisturizer")
 time.sleep(3)
-#check if have landed on the correct pages and click
-if(driver.find_element_by_xpath("//H2").text=="Moisturizers"):#change
+#check if have landed on the correct page
+
+#check if the heading of the page matches so that we can confirm we have landed on the right page
+if(driver.find_element_by_xpath("//H2").text=="Moisturizers"):
     print("Successfully entered the sunscreen shopping site")
 else:
     print("Failed to to enter the desired page.")
 time.sleep(2)
-prices=driver.find_elements_by_xpath("//P")
-print(prices)
+
+#find prices of all the elements
+prices=driver.find_elements_by_xpath("//p[contains(text(),'Price')]")
+
+#assign an initial maximum price and iterate comparing the prices of all the elements to the max price and assign the new maximum price
+max_price=1
+for i in range(6):
+    #get the integer value of price
+    prices_new=int(prices[i].text.split()[-1])
+    if(prices_new>max_price):
+        max_price=prices_new
+   
+print(max_price)
+#find the button corresponding to the max price and click it
+driver.find_element_by_xpath("(//P[contains(text(),'%d')])/following-sibling::BUTTON"%max_price).click()
