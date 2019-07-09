afdfbdc77512c61fd677011f94adcbe7a4e969f2
akkuldn <akkul.dn@qxf2.com>
2019-07-09 11:49:50 +0530

    Program to add most expensive moisturizer

--- Most_expensive_moisturizer.py
+++ Most_expensive_moisturizer.py
@@ -1,3 +1,18 @@
 """
  Program to add most expensive item from moisturizer page into the cart
 """
+import time
+from selenium import webdriver
+
+#create object for webdriver
+driver=webdriver.Chrome()
+driver.get("https://weathershopper.pythonanywhere.com/moisturizer")
+time.sleep(3)
+#check if have landed on the correct page
+if(driver.find_element_by_xpath("//H2").text=="Moisturizers"):
+    print("Successfully entered the sunscreen shopping site")
+else:
+    print("Failed to to enter the desired page.")
+time.sleep(2)
+prices=driver.find_elements_by_xpath("//P")
+print(prices)
